a937aac30436992c6e8820f0536eb34aeb2775f2
Edward Hoang <maitrongnghia.hoang@students.mq.edu.au>
2024-12-02 14:09:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Smooth Scroll for Internal Links and Open External Links in a New Tab
+document.querySelectorAll('a').forEach(link => {
+    if (link.href.startsWith("https://")) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            window.open(this.href, "_blank");
+        });
+    } else if (link.hash) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(this.hash).scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    }
+});
 
 const themeToggle = document.getElementById('theme-toggle');
 themeToggle.addEventListener('click', () => {
